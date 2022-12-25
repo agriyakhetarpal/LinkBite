4dc7c8a9e3431542119250d53736e9557cebb98e
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2022-12-25 20:50:49 +0530

    Start local app

--- shortener/config.py
+++ shortener/config.py
@@ -6,8 +6,11 @@ class Settings(BaseSettings):
     base_url: str = "https://localhost:8000"
     db_url: str = "sqlite:///./shortener.db"
 
+    class Config:
+        env_file = ".env"
+
 @lru_cache
 def get_settings() -> Settings:
     settings = Settings()
-    print(f"Loading settings for {settings.env_name}")
+    print(f"Loading settings for: {settings.env_name}")
     return settings
