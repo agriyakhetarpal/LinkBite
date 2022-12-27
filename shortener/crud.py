5d7b853b1cee556279e16a5de91de20a88680841
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2022-12-27 17:24:23 +0530

    Admin panel endpoint and more details for URLs

--- shortener/crud.py
+++ shortener/crud.py
@@ -13,3 +13,6 @@ def create_db_url(db: Session, url: schemas.URLBase) -> models.URL:
 # return key if it exists otherwise None
 def get_db_url_by_key(db: Session, url_key: str) -> models.URL:
     return (db.query(models.URL).filter(models.URL.key == url_key, models.URL.is_active).first())
+
+def get_db_url_by_secret_key(db: Session, secret_key: str) -> models.URL:
+    return (db.query(models.URL).filter(models.URL.secret_key == secret_key, models.URL.is_active).first())
--- shortener/main.py
+++ shortener/main.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI, HTTPException, Depends, Request
 from fastapi.responses import RedirectResponse
 
 import validators
+from starlette.datastructures import URL
 
 from . import models, schemas, crud
 from sqlalchemy.orm import Session
 from .database import SessionLocal, engine
+from .config import get_settings
 
 
 app = FastAPI()
@@ -42,6 +44,20 @@ def forward_to_target_url(
 def raise_bad_request(message):
     raise HTTPException(status_code=400, detail=message)
 
+@app.get("admin/{secret_key}", name="Admin panel", response_model=schemas.URLInfo)
+def get_url_info(secret_key: str, request = Request, db: Session = Depends(get_db)):
+    if db_url := crud.get_db_url_by_secret_key(db, secret_key=secret_key):
+        return get_admin_info(db_url)
+    else:
+        not_found_error("You are unauthorised to perform this action.")
+
+def get_admin_info(db_url:  models.URL) -> schemas.URLInfo:
+    base_url = URL(get_settings().base_url)
+    admin_endpoint = app.url_path_for("Admin panel", secret_key = db_url.secret_key)
+    db_url.url = str(base_url.replace(path = db_url.key))
+    db_url.admin_url = str(base_url.replace(path = admin_endpoint))
+    return db_url
+
 @app.post("/url", response_model=schemas.URLInfo)
 # to close session after finishing a request
 def create_url(url: schemas.URLBase, db:Session = Depends(get_db)):
@@ -50,7 +66,4 @@ def create_url(url: schemas.URLBase, db:Session = Depends(get_db)):
         raise_bad_request(message="Invalid URL")
 
     db_url = crud.create_db_url(db=db, url=url)
-    db_url.url = db_url.key
-    db_url.admin_url = db_url.secret_key
-    
-    return db_url 
+    return get_admin_info(db_url)
