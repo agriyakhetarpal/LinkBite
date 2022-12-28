85796f63fafff2de04d4dd2ac5b201500df565ea
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2022-12-28 17:57:26 +0530

    Keygen without string

--- shortener/keygen.py
+++ shortener/keygen.py
@@ -1,14 +1,17 @@
 import secrets
-import string
 
 from sqlalchemy.orm import Session
 from . import crud
 
+
 def create_key(length: int = 5) -> str:
-    chars = string.ascii_uppercase + string.digits
+    chars = "".join([chr(i) for i in range(65, 91)]) + "".join(
+        [chr(i) for i in range(48, 58)]
+    )
     return "".join(secrets.choice(chars) for _ in range(length))
 
-# repeat until key is unique by iterating over all existing keys 
+
+# repeat until key is unique by iterating over all existing keys
 def create_unique_key(db: Session) -> str:
     key = create_key()
     while crud.get_db_url_by_key(db, key):
